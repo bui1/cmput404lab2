aa963b3507f0222dc39701191187cd7552b36644
bui1@ualberta.ca <bui1@ualberta.ca>
2021-01-18 17:33:22 -0700

    modify proxy servers to use Process

--- proxy_client.py
+++ proxy_client.py
@@ -1,13 +1,13 @@
-from multiprocessing import Pool
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+payload = b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"
 
 # Connecting to a webpage using socket
 # stolen from vartec https://stackoverflow.com/users/60711/vartec
 # From StackOverflow
 # From https://stackoverflow.com/a/10600261
 s.connect(("localhost", 8001))
-s.sendall(b"GET HTTP/1.0\r\nHost: www.google.com\r\n\r\n")
+s.sendall(payload)
 print(s.recv(1024))
 s.close()
--- proxy_server.py
+++ proxy_server.py
@@ -1,19 +1,35 @@
+#!/usr/bin/env python3
 import socket
+from multiprocessing import Process
 
-# Stolen from https://docs.python.org/3/library/socket.html#example
-# Python3 socket docs to listen to connections and print data afterwards
-HOST = 'www.google.com'
-PORT = 80
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
-    s.listen(1)
-    connection, addr = s.accept()
 
-    with connection:
-        print('Connected by', addr)
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                break
-            connection.sendall(data)
+def thread(client_connection, client_addr):
+    client_request = client_connection.recv(1024)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('www.google.com', 80))
+    s.sendall(client_request)
+
+    while True:
+        data_from_server = s.recv(1024)
+        if (len(data_from_server) > 0):
+            # send response back to client after contacting webserver
+            client_connection.sendall(data_from_server)
+        else:
+            break
+
+    s.close()
+    client_connection.close()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('localhost', 8001))
+s.listen(2)
+
+while True:
+    client_connection, client_address = s.accept()
+    client_connection.settimeout(60)
+
+    p = Process(target=thread, args=(client_connection, client_address))
+    p.daemon = True
+    p.start()
